18ca45327109f1f82d1dd93016b43f343be12087
Rex Yu <yu19910513@gmail.com>
2021-04-12 12:29:06 -0700

    final

--- mini.js
+++ mini.js
@@ -9,13 +9,13 @@ var final = document.getElementById("final");
 var score = document.getElementById("score");
 var form = document.querySelector(".form");
 var rank = document.querySelector(".rank")
+var header = document.querySelector(".header")
 var optionA = document.querySelector(".optionA");
 var optionB = document.querySelector(".optionB");
 var optionC = document.querySelector(".optionC");
 var optionD = document.querySelector(".optionD");
-
+var img = document.getElementsByClassName("winImage");
 var loser = "You ran out of time! Game Over!!!";
-
 
 /* my calculator */
 function getSum(total, num) {
@@ -23,15 +23,21 @@ function getSum(total, num) {
 };
 
 function runOutOfTime() {
+    optionB.textContent = "Try Again?";
+    optionB.setAttribute("style", "background-color: black");
+    header.innerHTML = loser.fontcolor("red");
     notes.innerHTML = loser.fontcolor("red");
-    var hiddenArray = [optionB, optionC, optionD, optionA, start, timer, game];
+    var hiddenArray = [optionC, optionD, optionA, start, timer, game, warning];
     for (let i = 0; i < hiddenArray.length; i++) {
         hiddenArray[i].style.visibility = 'hidden';
-    }
+    };
+    optionB.onclick = function(){
+            location.reload();
+    };
 };
 
 /* timer */
-var countDown = 100;
+var countDown = 1100;
 var timerMajor = setInterval(function (){
     countDown--;
     timer.textContent = countDown + " seconds are left";
@@ -52,7 +58,7 @@ function wrongChoiceMajor() {
 
 /* main codes start here */
 
-var hiddenArray = [optionA, optionB, optionC, optionD];
+var hiddenArray = [optionA, optionB, optionC, optionD, game, timer];
 for (let i = 0; i < hiddenArray.length; i++) {
     hiddenArray[i].style.visibility = 'hidden';
 };
@@ -60,7 +66,7 @@ for (let i = 0; i < hiddenArray.length; i++) {
 rank.textContent = "The previous player, " + localStorage.getItem("person") + ", with a score of " + localStorage.getItem("score5");
 
 document.getElementById('start').onclick = function() {
-    var hiddenArray = [optionA, optionB, optionC, optionD];
+    var hiddenArray = [optionA, optionB, optionC, optionD, game, timer];
 for (let i = 0; i < hiddenArray.length; i++) {
     hiddenArray[i].style.visibility = 'visible';
 };
@@ -74,11 +80,13 @@ var wrongAnswer = [optionD, optionC, optionB];
 for (let i = 0; i < wrongAnswer.length; i++) {
     const element = wrongAnswer[i];
     wrongAnswer[i].onclick = function(){
+        element.setAttribute("style", "text-decoration: line-through; background-color: grey;");
         notes.textContent = "WRONG! Find the right one, so you can move on!";
         wrongChoice();
     };
 };
 optionA.onclick = function() {
+    optionA.setAttribute("style", "border-radius: 50%;");
     notes.textContent = "";
     winCount.textContent = "correct! " + countDown + " point(s) are accumulated! Press 'Next' to Continue!";
     localStorage.setItem("score1", countDown);
@@ -90,8 +98,10 @@ optionA.onclick = function() {
     document.getElementById('start').onclick = function() {
         var hiddenArray = [optionA, optionB, optionC, optionD];
         for (let i = 0; i < hiddenArray.length; i++) {
+            hiddenArray[i].removeAttribute("style", "text-decoration: line-through; background-color: grey;");
             hiddenArray[i].style.visibility = 'visible';
         };
+        optionA.setAttribute("style", "border-radius: 0%;");
         winCount.textContent = "";
         game.textContent = "What does JSON stand for?";
         optionA.textContent = "Java's Son";
@@ -103,11 +113,13 @@ optionA.onclick = function() {
     for (let i = 0; i < wrongAnswer.length; i++) {
         const element = wrongAnswer[i];
         wrongAnswer[i].onclick = function(){
+        element.setAttribute("style", "text-decoration: line-through; background-color: grey;");
         notes.textContent = "WRONG! Find the right one, so you can move on!";
         wrongChoice();
     };
 };
     optionC.onclick = function() {
+        optionC.setAttribute("style", "border-radius: 50%;");
         notes.textContent = "";
         var sumPrev = [localStorage.getItem("score1"), countDown];
         var scoreTwo = sumPrev.reduce(getSum, 0);
@@ -121,8 +133,10 @@ optionA.onclick = function() {
         document.getElementById('start').onclick = function() {
             var hiddenArray = [optionA, optionB, optionC, optionD];
             for (let i = 0; i < hiddenArray.length; i++) {
+                hiddenArray[i].removeAttribute("style", "text-decoration: line-through; background-color: grey;");
                 hiddenArray[i].style.visibility = 'visible';
             };
+            optionC.setAttribute("style", "border-radius: 0%;");
             winCount.textContent = "";
             game.textContent = "What does CSS stand for?";
             optionA.textContent = "Color & Style Sheets";
@@ -134,11 +148,13 @@ optionA.onclick = function() {
         for (let i = 0; i < wrongAnswer.length; i++) {
             const element = wrongAnswer[i];
             wrongAnswer[i].onclick = function(){
+            element.setAttribute("style", "text-decoration: line-through; background-color: grey;");
             notes.textContent = "WRONG! Find the right one, so you can move on!";
             wrongChoice();
             };
         };
         optionD.onclick = function() {
+            optionD.setAttribute("style", "border-radius: 50%;");
             notes.textContent = "";
             var sumPrev2 = [localStorage.getItem("score2"), countDown];
             var scoreThree = sumPrev2.reduce(getSum, 0);
@@ -152,8 +168,10 @@ optionA.onclick = function() {
             document.getElementById('start').onclick = function() {
                 var hiddenArray = [optionA, optionB, optionC, optionD];
                 for (let i = 0; i < hiddenArray.length; i++) {
+                    hiddenArray[i].removeAttribute("style", "text-decoration: line-through; background-color: grey;");
                     hiddenArray[i].style.visibility = 'visible';
                 };
+                optionD.setAttribute("style", "border-radius: 0%;");
                 winCount.textContent = "";
                 game.textContent = "What does API stand for?";
                 optionA.textContent = "Application Programming Interface";
@@ -165,11 +183,13 @@ optionA.onclick = function() {
             for (let i = 0; i < wrongAnswer.length; i++) {
                 const element = wrongAnswer[i];
                 wrongAnswer[i].onclick = function(){
+                element.setAttribute("style", "text-decoration: line-through; background-color: grey;");
                 notes.textContent = "WRONG! Find the right one, so you can move on!";
                 wrongChoice();
                 };
             };
             optionA.onclick = function() {
+                optionA.setAttribute("style", "border-radius: 50%;");
                 notes.textContent = "";
                 var sumPrev3 = [localStorage.getItem("score3"), countDown];
                 var scoreFour = sumPrev3.reduce(getSum, 0);
@@ -183,8 +203,10 @@ optionA.onclick = function() {
                 document.getElementById('start').onclick = function() {
                     var hiddenArray = [optionA, optionB, optionC, optionD];
                     for (let i = 0; i < hiddenArray.length; i++) {
+                        hiddenArray[i].removeAttribute("style", "text-decoration: line-through; background-color: grey;");
                         hiddenArray[i].style.visibility = 'visible';
                     };
+                    optionA.setAttribute("style", "border-radius: 0%;");
                     winCount.textContent = "";
                     game.textContent = "How many hours [per week] should a bootcamp student usually spend to practice coding outside the class?";
                     optionA.textContent = "0";
@@ -196,31 +218,41 @@ optionA.onclick = function() {
                 for (let i = 0; i < wrongAnswer.length; i++) {
                     const element = wrongAnswer[i];
                     wrongAnswer[i].onclick = function(){
+                    element.setAttribute("style", "text-decoration: line-through; background-color: grey;");
                     notes.textContent = "WRONG! You do not have much time left!";
                     wrongChoiceMajor();
                     warning.textContent = "A wrong answer deducts more time than before in this round by the way! 😈";
                     };
                 };
+
+                optionC.addEventListener("click", function(){
+                var visibleArray = [optionA, optionB];
+                    for (let i = 0; i < hiddenArray.length; i++) {
+                        visibleArray[i].removeAttribute("style", "text-decoration: line-through; background-color: grey;");
+                    };
+                });
+
                 optionC.onclick = function() {
+                    img[0].classList.remove("winImage");
+                    optionC.setAttribute("style", "border-radius: 50%;");
                     notes.textContent = "";
                     var sumPrev4 = [localStorage.getItem("score4"), countDown];
                     var scoreFive = sumPrev4.reduce(getSum, 0);
-                    game.textContent = "correct! This is the end of the quiz!";
+                    score.textContent = "This is the end of the quiz!";
                     clearInterval(timerMajor);
                     var finalScore = scoreFive * countDown;
                     localStorage.setItem("score5", finalScore);
-                    final.textContent = "Your accumulated score is " + scoreFive + " , and you still have " + countDown + " seconds left.";
-                    score.textContent = "Your final score is " + finalScore + "!";
+                    rank.textContent = "Your accumulated score is " + scoreFive + " , and you still have " + countDown + " seconds left.";
+                    game.textContent = "Your final score is " + finalScore + "!";
+                    rank.setAttribute("style", "text-decoration: none; background-color: black; color: white")
                     var hiddenArray = [optionC, optionD, start];
                     for (let i = 0; i < hiddenArray.length; i++) {
                         hiddenArray[i].style.visibility = 'hidden';
-                    }
+                    };
                     optionA.textContent = "Log Your Score!";
                     optionB.textContent = "Play Again!";
                     optionB.onclick = function(){
-                        if (confirm("Thank you for playing!")){
-                            location.reload();
-                        };
+                        location.reload();
                     };
                     optionA.onclick = function() {
                         var person = (prompt("Please enter your name"));
